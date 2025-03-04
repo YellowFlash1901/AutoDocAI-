4154fa5e4929909e2e2429e4ea4d8a9891e98ff6
YellowFlash1901 <vardaansathe@gmail.com>
2025-03-04 23:50:28 +0000

    done with getting entire tree struc with sha but remaining with getting last string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from src.parser import parse_owner_repo
+from src.parser import parse_owner_repo , fetch_repo_sha , fetch_repo_tree
 
 app = FastAPI()
 
@@ -9,6 +9,8 @@ app = FastAPI()
 def get_file_structure(url: str):
     #first is to get owner then repo name then sha then repo json.
     #then get the tree sha from the repo json
-    owner, repo = parse_owner_repo(url)
-
-    return {"owner": owner, "repo": repo}
+    owner, repo, last_string = parse_owner_repo(url)
+    sha = fetch_repo_sha(owner, repo)
+    tree = fetch_repo_tree(owner, repo, sha)
+    return {"owner": owner, "repo": repo, "last_string": last_string, "sha": sha, "tree": tree}
+     
--- src/parser.py
+++ src/parser.py
@@ -1,13 +1,34 @@
 import re
 import urllib.parse
+import requests
 
 def parse_owner_repo(url: str):
     decoded_url = urllib.parse.unquote(url)
-    pattern = r"https://github\.com/(?P<owner>[^/]+)/(?P<repo>[^/]+)"
+    pattern = r"https://github\.com/(?P<owner>[^/]+)/(?P<repo>[^/]+)(/(?P<lastString>.+))?"
     match = re.match(pattern, decoded_url)
     if match:
         owner = match.group("owner")
         repo = match.group("repo")
-        return owner, repo
-    return None, None
+        last_string = match.group("lastString") or ''
 
+        return owner, repo, last_string
+    return None, None, None
+
+def fetch_repo_sha(owner: str, repo: str):
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if isinstance(data, list) and len(data) > 0:
+            return data[-1].get('sha')
+    return None
+
+def fetch_repo_tree(owner: str, repo: str, sha: str):
+    url = f"https://api.github.com/repos/{owner}/{repo}/git/trees/{sha}?recursive=1"
+    print(url)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print("fetch repo",data)
+        return data.get('tree')          
+    return None
